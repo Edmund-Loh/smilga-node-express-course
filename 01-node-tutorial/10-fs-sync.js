a353724ed449fc9183b5c42eb9baa7902c381ae6
Edmund Loh <edmund.wq.loh@gmail.com>
2022-08-05 00:40:29 +0800

    tutorial practice

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -1,12 +1,17 @@
 const { readFileSync, writeFileSync } = require('fs')
 console.log('start')
+
 const first = readFileSync('./content/first.txt', 'utf8')
+console.log('first.txt has been read')
+
 const second = readFileSync('./content/second.txt', 'utf8')
+console.log('second.txt has been read')
 
 writeFileSync(
   './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`,
+  `Here is the result : ${first}, ${second} \n`,
   { flag: 'a' }
-)
+)//flag: 'a' will cause append to result-sync.txt
+
 console.log('done with this task')
 console.log('starting the next one')

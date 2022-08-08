aefa0d9d2065d1e65fb80a0a28985502e5fdccb5
Edmund Loh <edmund.wq.loh@gmail.com>
2022-08-09 00:01:02 +0800

    e-learning

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -1,13 +1,13 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  //   if (req.url === '/') {
+const server = http.createServer((request, response) => {
+  //   if (request.url === '/') {
   //     res.end('Welcome to our home page')
   //   }
-  //   if (req.url === '/about') {
+  //   if (request.url === '/about') {
   //     res.end('Here is our short history')
   //   }
-  //   res.end(`
+  //   response.end(`
   //   <h1>Oops!</h1>
   // <p>We can't seem to find the page you are looking for</p>
   // <a href="/">back home</a>
@@ -18,12 +18,12 @@ const server = http.createServer((req, res) => {
   //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
   // SWITCH TO IF, ELSE IF, ELSE (BELOW)
   // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
-  if (req.url === '/') {
-    res.end('Welcome to our home page')
+  if (request.url === '/') {
+    response.end('Welcome to our home page')
   } else if (req.url === '/about') {
-    res.end('Here is our short history')
+    response.end('Here is our short history')
   } else {
-    res.end(`
+    response.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">back home</a>

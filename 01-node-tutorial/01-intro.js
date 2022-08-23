f3fd0c1811f4bb413b8cbc8017d722f0c163428c
Edmund Loh <edmund.wq.loh@gmail.com>
2022-08-24 00:04:23 +0800

    testing

--- 01-node-tutorial/01-intro.js
+++ 01-node-tutorial/01-intro.js
@@ -7,3 +7,18 @@ if (amount < 10) {
 }
 
 console.log(`hey it's my first node app!!!`)
+
+
+let promise = new Promise (
+  resolve => {
+    setTimeout(() => resolve('done!'),1000)
+  }
+)
+
+promise.then(
+  (result) => {
+    console.log(result)
+  }
+);
+
+console.log('Testing... Testing...')

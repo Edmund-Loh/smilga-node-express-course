b0ffc90dd34428a55cbfad21635973507278d47b
Edmund Loh <edmund.wq.loh@gmail.com>
2022-08-29 00:05:44 +0800

    elearning

--- 01-node-tutorial/13-event-emitter.js
+++ 01-node-tutorial/13-event-emitter.js
@@ -1,9 +1,9 @@
 // get back the class
 // if want custom extend from class
 // otherwise just for emitting and handling events create instance
-const EventEmitter = require('events')
+let EventEmitter = require('events');
 
-const customEmitter = new EventEmitter()
+let customEmitter = new EventEmitter();
 
 // on and emit methods
 // keep track of the order
@@ -11,11 +11,21 @@ const customEmitter = new EventEmitter()
 // built-in modules utilize it
 
 customEmitter.on('response', (name, id) => {
-  console.log(`data recieved user ${name} with id:${id}`)
+  console.log(`data recieved user ${name} with id:${id}`);
 })
 
-customEmitter.on('response', () => {
-  console.log('some other logic here')
+customEmitter.on('event', () => {
+  console.log('some other logic here');
 })
 
-customEmitter.emit('response', 'john', 34)
+customEmitter.on('some other event', (...args) => {
+  console.log('a listener for some other event...');
+  let params = args.join(', ');
+  console.log(`It has these params: ${params}`);
+})
+
+customEmitter.emit('response', 'john', 34);
+
+customEmitter.emit('event');
+
+customEmitter.emit('some other event', 1, 2, 3, 4, 5, 6, 7);
